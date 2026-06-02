e5ef23d450a91040976a77148a48e6d066b8d617
Hima Harika <himaharika2827@example.com>
2026-06-02 23:44:30 +0530

    postgres production setup

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'shortify.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URL",
+    "sqlite:///shortify.db"
+)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
